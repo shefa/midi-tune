22546361dd1a8f9277adeebeb2eafeef711e6921
Georgi Marinov <venom1724@gmail.com>
2020-06-02 10:50:38 +0100

    update

--- ML/bigbrain.py
+++ ML/bigbrain.py
@@ -7,74 +7,89 @@ import sys
 import random
 from collections import deque
 from keras.utils import to_categorical
+from keras.callbacks import Callback
+import wandb
 
 def toolbar_init(toolbar_width, cnt):
-	sys.stdout.write("[%s]" % (" " * toolbar_width))
-	sys.stdout.flush()
-	sys.stdout.write("\b" * (toolbar_width+1))
-	return int((cnt+toolbar_width-1)/toolbar_width)
+    sys.stdout.write("[%s]" % (" " * toolbar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width+1))
+    return int((cnt+toolbar_width-1)/toolbar_width)
 
 def toolbar_tick(smth):
-	if not smth:
-		sys.stdout.write("-")
-		sys.stdout.flush()
+    if not smth:
+        sys.stdout.write("-")
+        sys.stdout.flush()
+
+def batch_generator(Train_df, batch_size,steps):
+    idx=1
+    while True: 
+        yield load_data(Train_df,idx-1,batch_size)## Yields data
+        if idx<steps:
+            idx+=1
+        else:
+            idx=1
+
+class CustomCallback(Callback):
+    def on_train_batch_end(self, batch, logs=None):
+        wandb.log({'accuracy': logs['accuracy'], 'loss': logs['loss']})
 
 def make_sequences_basic(data,sequence_length):
-	s_in = []
-	s_out = []
-	sequences = []
-	prev_notes = deque(maxlen=sequence_length)
-	cnt, sz = 0, len(data)
-	for notes in data:
-		print(f'{cnt}/{sz}')
-		cnt+=1
-		for i in notes:
-			if len(prev_notes) == sequence_length:
-				sequences.append([np.array(prev_notes),i])
-			prev_notes.append(i)
+    s_in = []
+    s_out = []
+    sequences = []
+    prev_notes = deque(maxlen=sequence_length)
+    cnt, sz = 0, len(data)
+    for notes in data:
+        print(f'{cnt}/{sz}')
+        cnt+=1
+        for i in notes:
+            if len(prev_notes) == sequence_length:
+                sequences.append([np.array(prev_notes),i])
+            prev_notes.append(i)
 
-	random.shuffle(sequences)
+    random.shuffle(sequences)
 
-	for i,o in sequences:
-		s_in.append(i)
-		s_out.append(o)
+    for i,o in sequences:
+        s_in.append(i)
+        s_out.append(o)
 
-	n_patterns = len(s_in)
+    s_in = np.array(s_in, dtype=np.int8)
+    s_out = np.array(s_out, dtype=np.int8)
+    n_patterns = len(s_in)
+    s_in = np.reshape(s_in, (n_patterns, sequence_length, 1))
+    s_out = np.reshape(s_out, (n_patterns, 1))
 
-	s_in = np.array(s_in, dtype=np.int8)
-	s_in = np.reshape(s_in, (n_patterns, sequence_length, 1))
-	s_out = np.array(s_out, dtype=np.int8)
-	s_out = np.reshape(s_out, (n_patterns, 1))
-	return s_in, s_out #to_categorical(s_out,num_classes=88, dtype=np.bool)
+    return s_in, s_out #to_categorical(s_out,num_classes=88, dtype=np.bool)
 
 
 def vibe_check(d):
-	print(max(d), min(d))
-	print(np.mean(d), np.median(d))
-	print(np.quantile(d,.25), np.quantile(d,.75), np.quantile(d,.95))
-	m_value=max(d)#1000
-	plt.hist(d,range(m_value), density=True)
-	plt.gca().set(title='Frequency histogram for Delta time values', ylabel='Frequency')
-	plt.xlim(0,m_value)
-	plt.legend()
-	plt.show()
+    print(max(d), min(d))
+    print(np.mean(d), np.median(d))
+    print(np.quantile(d,.25), np.quantile(d,.75), np.quantile(d,.95))
+    m_value=max(d)#1000
+    plt.hist(d,range(m_value), density=True)
+    plt.gca().set(title='Frequency histogram for Delta time values', ylabel='Frequency')
+    plt.xlim(0,m_value)
+    plt.legend()
+    plt.show()
 
 def ticks_per_beat_test():
-	switches=[132, 914, 1183]
-	four_eighty = [ [x[2] for y in thing[:switches[0]] for x in y], [x[2] for y in thing[switches[1]:switches[2]] for x in y] ]
-	three_eighty = [ [x[2] for y in thing[switches[0]:switches[1]] for x in y], [x[2] for y in thing[switches[2]:] for x in y] ]
+    switches=[132, 914, 1183]
+    four_eighty = [ [x[2] for y in thing[:switches[0]] for x in y], [x[2] for y in thing[switches[1]:switches[2]] for x in y] ]
+    three_eighty = [ [x[2] for y in thing[switches[0]:switches[1]] for x in y], [x[2] for y in thing[switches[2]:] for x in y] ]
 
-	four=four_eighty[0]+four_eighty[1]
-	three=three_eighty[0]+three_eighty[1]
+    four=four_eighty[0]+four_eighty[1]
+    three=three_eighty[0]+three_eighty[1]
 
-	print(np.mean(four), np.mean(three))
-	print(np.median(four), np.median(three))
+    print(np.mean(four), np.mean(three))
+    print(np.median(four), np.median(three))
 
-	m_value=1000
-	m_value=max(four+three)
-	plt.hist(three,range(m_value), alpha=0.5, label='384 ticks', density=True)
-	plt.hist(four,range(m_value),  alpha=0.5, label='480 ticks', density=True)
-	plt.gca().set(title='Frequency histogram for Delta time with different ticks_per_beat', ylabel='Frequency')
-	plt.xlim(0,500)
-	plt.legend()
-	plt.show()
+    m_value=1000
+    m_value=max(four+three)
+    plt.hist(three,range(m_value), alpha=0.5, label='384 ticks', density=True)
+    plt.hist(four,range(m_value),  alpha=0.5, label='480 ticks', density=True)
+    plt.gca().set(title='Frequency histogram for Delta time with different ticks_per_beat', ylabel='Frequency')
+    plt.xlim(0,500)
+    plt.legend()
+    plt.show()
--- ML/model.py
+++ ML/model.py
@@ -12,17 +12,19 @@ import wandb
 from wandb.keras import WandbCallback
 
 from bigbrain import make_sequences_basic as make_sequences
+from bigbrain import CustomCallback
 
 hyperparameter_defaults = dict(
-  dropout = 0.2,
-  hidden_layer_size = 64,
+  dropout = 0.3,
+  hidden_layer_size = 128,
   layer_1_size = 128,
   layer_2_size = 128,
   layer_3_size = 128,
   learn_rate = 0.01,
   decay = 1e-6,
   epochs = 8,
-  sequence_length = 100,
+  batch_size = 256,
+  sequence_length = 200,
   input_data_type = 0,
 )
 
@@ -38,35 +40,35 @@ data_type=typemap[config.input_data_type]
 
 #load data
 def load_data():
-	train_x = 		pickle.load(open(f"{sequence_folder}trainx-{data_type}",		'rb'))
-	train_y = 		pickle.load(open(f"{sequence_folder}trainy-{data_type}",		'rb'))
-	test_x =  		pickle.load(open(f"{sequence_folder}testx-{data_type}",			'rb'))
-	test_y =  		pickle.load(open(f"{sequence_folder}testy-{data_type}",			'rb'))
-	validation_x =  pickle.load(open(f"{sequence_folder}validationx-{data_type}",	'rb'))
-	validation_y =  pickle.load(open(f"{sequence_folder}validationy-{data_type}",	'rb'))
-	return train_x, train_y, test_x, test_y, validation_x, validation_y
+    train_x =       pickle.load(open(f"{sequence_folder}trainx-{data_type}",        'rb'))
+    train_y =       pickle.load(open(f"{sequence_folder}trainy-{data_type}",        'rb'))
+    test_x =        pickle.load(open(f"{sequence_folder}testx-{data_type}",         'rb'))
+    test_y =        pickle.load(open(f"{sequence_folder}testy-{data_type}",         'rb'))
+    validation_x =  pickle.load(open(f"{sequence_folder}validationx-{data_type}",   'rb'))
+    validation_y =  pickle.load(open(f"{sequence_folder}validationy-{data_type}",   'rb'))
+    return train_x, train_y, test_x, test_y, validation_x, validation_y
 
 def create_data():
-	print("Loading dataset..")
-	data_parsed = [pickle.load(open(f'{data_folder}rick-{data_type}-{x}','rb')) for x in data_split]
-	print("generating train..")
-	train_x, train_y = make_sequences(data_parsed[0], config.sequence_length)
-	print("generating test..")
-	test_x, test_y = make_sequences(data_parsed[2], config.sequence_length)
-	print("generating validation..")
-	validation_x, validation_y = make_sequences(data_parsed[1], config.sequence_length)
-	return train_x, train_y, test_x, test_y, validation_x, validation_y
+    print("Loading dataset..")
+    data_parsed = [pickle.load(open(f'{data_folder}rick-{data_type}-{x}','rb')) for x in data_split]
+    print("generating train..")
+    train_x, train_y = make_sequences(data_parsed[0][:400], config.sequence_length)
+    print("generating test..")
+    test_x, test_y = make_sequences(data_parsed[2][400:500], config.sequence_length)
+    print("generating validation..")
+    validation_x, validation_y = make_sequences(data_parsed[1][-100:], config.sequence_length)
+    return train_x, train_y, test_x, test_y, validation_x, validation_y
 
 def save_data():
-	print("saving train..")
-	pickle.dump(train_x,open(f"{sequence_folder}trainx-{data_type}",'wb'))
-	pickle.dump(train_y,open(f"{sequence_folder}trainy-{data_type}",'wb'))
-	print("saving test..")
-	pickle.dump(test_x,open(f"{sequence_folder}testx-{data_type}",'wb'))
-	pickle.dump(test_y,open(f"{sequence_folder}testy-{data_type}",'wb'))
-	print("saving validation..")
-	pickle.dump(validation_x,open(f"{sequence_folder}validationx-{data_type}",'wb'))
-	pickle.dump(validation_y,open(f"{sequence_folder}validationy-{data_type}",'wb'))
+    print("saving train..")
+    pickle.dump(train_x,open(f"{sequence_folder}trainx-{data_type}",'wb'))
+    pickle.dump(train_y,open(f"{sequence_folder}trainy-{data_type}",'wb'))
+    print("saving test..")
+    pickle.dump(test_x,open(f"{sequence_folder}testx-{data_type}",'wb'))
+    pickle.dump(test_y,open(f"{sequence_folder}testy-{data_type}",'wb'))
+    print("saving validation..")
+    pickle.dump(validation_x,open(f"{sequence_folder}validationx-{data_type}",'wb'))
+    pickle.dump(validation_y,open(f"{sequence_folder}validationy-{data_type}",'wb'))
 
 train_x, train_y, test_x, test_y, validation_x, validation_y = create_data()
 print("Sequences loaded")
@@ -76,7 +78,6 @@ model = Sequential()
 model.add(LSTM(config.layer_1_size,input_shape=(train_x.shape[1], train_x.shape[2]),return_sequences=True, recurrent_dropout=config.dropout))
 #model.add(LSTM(config.layer_2_size, return_sequences=True, recurrent_dropout=config.dropout))
 model.add(LSTM(config.layer_3_size))
-model.add(Dropout(config.dropout))
 model.add(Dense(config.hidden_layer_size, activation='relu'))
 model.add(Dropout(config.dropout))
 model.add(Dense(88, activation='softmax'))
@@ -85,12 +86,14 @@ opt = Adam(lr=config.learn_rate, decay=config.decay)
 atm = str(time.strftime("%H-%M"))
 model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 try:
-	model.fit(train_x, train_y,  validation_data=(test_x, test_y), epochs=config.epochs,callbacks=[WandbCallback()])
+    model.fit(train_x, train_y,  validation_data=(test_x, test_y), epochs=config.epochs, batch_size=config.batch_size, callbacks=[WandbCallback(), CustomCallback()])
 except KeyboardInterrupt:
-	score = model.evaluate(validation_x, validation_y, verbose=0)
-	print('Test loss:', score[0])
-	print('Test accuracy:', score[1])
+    print("-------------- Something happen -------")
+    score = model.evaluate(validation_x, validation_y, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
 finally:
-	# Save model
-	model.save(f"models/{data_type}-{atm}")
-	tfjs.converters.save_keras_model(model, f"models/js/{data_type}-{atm}")
+    # Save model
+    print("-------------- Saving model ---------- ")
+    model.save(f"models/{data_type}-{atm}")
+    tfjs.converters.save_keras_model(model, f"models/js/{data_type}-{atm}")
